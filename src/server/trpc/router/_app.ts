21e774394e5519e1d95fdf952f6b50d04331b51b
hydrasaur <hydra50321@mgail.com>
2023-01-04 15:06:56 +0100

    Fix example routing

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -1,9 +1,8 @@
 import { router } from "../trpc";
 import { authRouter } from "./auth";
-import { exampleRouter } from "./example";
 import { logRouter } from "./log";
+
 export const appRouter = router({
-  example: exampleRouter,
   auth: authRouter,
   log: logRouter,
 });
